da9701c4d9019d44da85a527dc30495d06be387e
Hass <hassmroueh@gmail.com>
2024-02-08 15:45:03 -0500

    scope lab complete

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your solution in this file!
+var customerName = ("bob")
+function customerName(){
+  return "bob"
+}
+function upperCaseCustomerName(){
+  customerName=("BOB")
+}
+
+function setBestCustomer() {
+  window.bestCustomer = 'not bob';
+}
+function overwriteBestCustomer(){
+  window.bestCustomer="maybe bob"
+}
+
+function changeLeastFavoriteCustomer(newCustomer) {
+  throw new Error('Assignment to constant variable.');
+}
